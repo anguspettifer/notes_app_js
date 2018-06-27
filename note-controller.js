5e635e01854e92b106ec9c065971dc730117225f
Simone <simonecaplin@gmail.com>
2018-06-27 09:04:15 +0100

    Spiking code to show a note in index.html

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,24 @@
-function changeText() {
-  var element = document.getElementById('app')
-  element.innerHTML = "howdy"
-}
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteList = noteList;
+    this.noteList.addNote("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(noteList)
+  };
 
-changeText()
+  NoteController.prototype.getHTML = function () {
+    var element = document.getElementById('app')
+    element.innerHTML = this.noteListView.returnHTML()
+  };
+
+
+
+  exports.NoteController = NoteController;
+})(this);
+
+
+
+// function changeText() {
+//   var element = document.getElementById('app')
+//   element.innerHTML = "howdy"
+// }
+// changeText()
--- note-tests.js
+++ note-tests.js
@@ -20,7 +20,6 @@ function testNoteListView() {
   var noteList = new NoteList();
   noteList.addNote("First note");
   noteList.addNote("Second note");
-  console.log(noteList);
   var noteListView = new NoteListView(noteList);
   assert.isTrue(noteListView.returnHTML().includes("<ul><li><div>First note</div></li><li><div>Second note</div></li></ul>"));
 };
